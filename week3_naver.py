098df9862b5002334977edd56f982cf4e5afd8ea
hyuk7474 <65122201+hyuk7474@users.noreply.github.com>
2020-05-28 22:31:44 +0900

    몽고 DB 연동

--- week3_naver.py
+++ week3_naver.py
@@ -1,6 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+
+
+client = MongoClient('localhost', 27017) # client가 robo와 같은 역할(mongodb에 연결)
+db = client.dbsparta # 새로운 데이터베이스 추가 # dbsparta라는 이름의 데이터베이스가 있으면 가져오고 없으면 추가(get or create)
+#데이터 변수 명은 1)숫자로 시작 불가 2)for로 명명 불가 3)숫자로 시작 불가
+
+# db.users.insert_one({'name': 'bobby', 'age': 21})
+
+# mongodb 가져오기
+users = list(db.users.find()) # users는 리스트
+# age 값이 21인 오브젝트만 가져오기
+users = list(db.users.find({'age': 21}))
+
+for user in users:
+    print(user['name'])
+
+user = db.users.find_one({'name': 'bobby'}, {'_id': False})
+# 특정한 오브젝트 하나만 찾아보기
+# 뒷부분의, {'_id': False} 부분은 되도록 함께 쓰도록 하기
+
+
 # 크롤링 하고 싶은 사이트 URL
 target_url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303'
 
@@ -13,7 +35,7 @@ data = requests.get(target_url, headers=headers)
 # 이제 코딩을 통해 필요한 부분을 추출하면 된다.
 soup = BeautifulSoup(data.text, 'html.parser')
 
-print(soup)
+# print(soup)
 
 # $('#cards-box') jquery를 이용해 특정 html 태그의 정보를 가져옴
 # .class, #cards-box, div > h1 -> CSS 선택자(셀렉터)
@@ -42,12 +64,21 @@ for movie in movies: # 여러 개의 tr 태그를 순서대로 순회
         rank = rank_tag['alt']
         text = a_tag.text #태그의 값을 가져옴 <a>(값)</a>
         point = point_tag.text
-        print(int(rank), text, point)
+        
+     
+        # print(int(rank), text, point)
+        document = {
+            'rank': int(rank),
+            'title': text,
+            'point': point,
+        }
+        # if db.user.document['title'] is '매트릭스':
+        # return print(db.user.document['point'])
+target_movie = db.user.document.find_one({'title': '매트릭스'})
+print(target_movie)['point']
 
+
+        # db.users.insert_one(document)
         #숫자를 문자열로 str()
-        #문자열을 숫자로 int(), 숫자가 아닌 문자는
+        #문자열을 숫자로 int(), 숫자가 아닌 문자는 
 
-#old_content > table > tbody > tr > td.point
-#old_content > table > tbody > tr:nth-child(2) > td.title > div > a
-#old_content > table > tbody > tr > td > img
-#old_content > table > tbody > tr:nth-child(3) > td:nth-child(1) > img
