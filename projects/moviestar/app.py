0b7820a0e9c4f4e00abb3b795897a515c0df27cd
hyuk7474 <65122201+hyuk7474@users.noreply.github.com>
2020-06-11 21:09:20 +0900

    스타 삭제 기능

--- projects/moviestar/app.py
+++ projects/moviestar/app.py
@@ -35,7 +35,7 @@ def star_like():
     new_like = star['like'] + 1
     # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
     # 참고: '$set' 활용하기!
-    db.mystar.update_one({'name': name_receive}, {'$Set': {'like': new_like}})
+    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
     # 5. 성공하면 success 메시지를 반환합니다.
 
     return jsonify({'result': 'success'})
@@ -44,9 +44,12 @@ def star_like():
 @app.route('/api/delete', methods=['POST'])
 def star_delete():
     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    name_receive = request.form['name_give']
     # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
+    star = db.mystar.delete_one({'name': name_receive})
+
     # 3. 성공하면 success 메시지를 반환합니다.
-	return jsonify({'result': 'success','msg':'delete 연결되었습니다!'})
+    return jsonify({'result': 'success','msg':'delete 연결되었습니다!'})
 
 
 if __name__ == '__main__':
